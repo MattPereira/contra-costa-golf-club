265b6191222914ee3d9e9e158175381a75c81180
Matthew Pereira <matthewdavidpereira@gmail.com>
2023-05-19 20:57:29 -0700

    cleanup database folder

--- database/reset.sql
+++ database/reset.sql
@@ -1,11 +1,8 @@
-/* Delete databases and re-seed with data */
-
 DROP DATABASE IF EXISTS ccgc;
 CREATE DATABASE ccgc;
 \connect ccgc;
 
 \i ccgc-schema.pgsql
-\i ccgc-seed.pgsql
 
 
 DROP DATABASE IF EXISTS ccgc_test;
